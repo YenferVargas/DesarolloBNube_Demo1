4ae8b741925dc5198fc40b7c9ae63b6d5cb9a4c2
yenif <yenifer.vargas@tecsup.edu.pe>
2023-09-05 10:23:23 -0500

    "cambios_01"

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
-	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
+	<meta name="author" content="Sergey Pozhilov (GetTemplate.com)">
 	
-	<title>Progressus - Free business bootstrap template by GetTemplate</title>
+	<title>Progressus - Plantilla gratuita de Bootstrap para negocios por GetTemplate</title>
 
 	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
 	
@@ -14,11 +14,11 @@
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 
-	<!-- Custom styles for our template -->
-	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+	<!-- Estilos personalizados para nuestra plantilla -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
 	<link rel="stylesheet" href="assets/css/main.css">
 
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- Soporte de HTML5 shim y Respond.js para IE8 -->
 	<!--[if lt IE 9]>
 	<script src="assets/js/html5shiv.js"></script>
 	<script src="assets/js/respond.min.js"></script>
@@ -26,73 +26,75 @@
 </head>
 
 <body class="home">
-	<!-- Fixed navbar -->
-	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+	<!-- Barra de navegación fija -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom">
 		<div class="container">
 			<div class="navbar-header">
-				<!-- Button for smallest screens -->
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-				<a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" alt="Progressus HTML5 template"></a>
+				<!-- Botón para las pantallas más pequeñas -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" alt="Plantilla HTML5 Progressus"></a>
 			</div>
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav pull-right">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="about.php">About</a></li>
+					<li class="active"><a href="#">Inicio</a></li>
+					<li><a href="about.php">Acerca de</a></li>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">More Pages <b class="caret"></b></a>
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Más Páginas <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="sidebar-left.php">Left Sidebar</a></li>
-							<li class="active"><a href="sidebar-right.php">Right Sidebar</a></li>
+							<li><a href="sidebar-left.php">Barra lateral izquierda</a></li>
+							<li class="active"><a href="sidebar-right.php">Barra lateral derecha</a></li>
 						</ul>
 					</li>
-					<li><a href="contact.php">Contact</a></li>
-					<li><a class="btn" href="signin.php">SIGN IN / SIGN UP</a></li>
+					<li><a href="contact.php">Contacto</a></li>
+					<li><a class="btn" href="signin.php">INICIAR SESIÓN / REGISTRARSE</a></li>
 				</ul>
 			</div><!--/.nav-collapse -->
 		</div>
-	</div> 
+	</div>
 	<!-- /.navbar -->
 
-	<!-- Header -->
+	<!-- Encabezado -->
 	<header id="head">
 		<div class="container">
 			<div class="row">
-				<h1 class="lead">AWESOME, CUSTOMIZABLE, FREE</h1>
-				<p class="tagline">PROGRESSUS: free business bootstrap template by <a href="http://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a></p>
-				<p><a class="btn btn-default btn-lg" role="button">MORE INFO</a> <a class="btn btn-action btn-lg" role="button">DOWNLOAD NOW</a></p>
+				<h1 class="lead">BIENVENIDO A DISEÑO DE DESARROLLO DE SOFTWARE</h1>
+				<p class="tagline">PROGRESSUS: plantilla de Bootstrap gratuita para negocios por <a href="http://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a></p>
+				<p><a class="btn btn-default btn-lg" role="button">MÁS INFORMACIÓN</a> <a class="btn btn-action btn-lg" role="button">DESCARGAR AHORA</a></p>
 			</div>
 		</div>
 	</header>
-	<!-- /Header -->
+	<!-- /Encabezado -->
 
-	<!-- Intro -->
+	<!-- Introducción -->
 	<div class="container text-center">
 		<br> <br>
-		<h2 class="thin">The best place to tell people why they are here</h2>
+		<h2 class="thin">El mejor lugar para decirle a la gente por qué están aquí</h2>
 		<p class="text-muted">
-			The difference between involvement and commitment is like an eggs-and-ham breakfast:<br> 
-			the chicken was involved; the pig was committed.
+			La diferencia entre la implicación y el compromiso es como un desayuno de huevos y jamón:<br> 
+			la gallina estaba involucrada; el cerdo estaba comprometido.
 		</p>
 	</div>
-	<!-- /Intro-->
-		
-	<!-- Highlights - jumbotron -->
+	<!-- /Introducción -->
+
+	<!-- Destacados - jumbotron -->
 	<div class="jumbotron top-space">
 		<div class="container">
 			
-			<h3 class="text-center thin">Reasons to use this template</h3>
+			<h3 class="text-center thin">Razones para usar esta plantilla</h3>
 			
 			<div class="row">
 				<div class="col-md-3 col-sm-6 highlight">
-					<div class="h-caption"><h4><i class="fa fa-cogs fa-5"></i>Bootstrap-powered</h4></div>
+					<div class="h-caption"><h4><i class="fa fa-cogs fa-5"></i>Alimentado por Bootstrap</h4></div>
 					<div class="h-body text-center">
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aliquid adipisci aspernatur. Soluta quisquam dignissimos earum quasi voluptate. Amet, dignissimos, tenetur vitae dolor quam iusto assumenda hic reprehenderit?</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 highlight">
-					<div class="h-caption"><h4><i class="fa fa-flash fa-5"></i>Fat-free</h4></div>
+					<div class="h-caption"><h4><i class="fa fa-flash fa-5"></i>Sin grasa</h4></div>
 					<div class="h-body text-center">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, commodi, sequi quis ad fugit omnis cumque a libero error nesciunt molestiae repellat quos perferendis numquam quibusdam rerum repellendus laboriosam reprehenderit! </p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, commodi, sequi quis ad fugit omnis cumque a libero error nesciunt molestiae repellat quos perferendis numquam quibusdam rerum repellendus laboriosam reprehenderit!</p>
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 highlight">
@@ -102,84 +104,80 @@
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-6 highlight">
-					<div class="h-caption"><h4><i class="fa fa-smile-o fa-5"></i>Author's support</h4></div>
+					<div class="h-caption"><h4><i class="fa fa-smile-o fa-5"></i>Soporte del autor</h4></div>
 					<div class="h-body text-center">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, excepturi, maiores, dolorem quasi reprehenderit illo accusamus nulla minima repudiandae quas ducimus reiciendis odio sequi atque temporibus facere corporis eos expedita? </p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, excepturi, maiores, dolorem quasi reprehenderit illo accusamus nulla minima repudiandae quas ducimus reiciendis odio sequi atque temporibus facere corporis eos expedita?</p>
 					</div>
 				</div>
 			</div> <!-- /row  -->
 		
 		</div>
 	</div>
-	<!-- /Highlights -->
+	<!-- /Destacados -->
 
-	<!-- container -->
+	<!-- Contenedor -->
 	<div class="container">
-
-		<h2 class="text-center top-space">Frequently Asked Questions</h2>
+		<h2 class="text-center top-space">Preguntas frecuentes</h2>
 		<br>
 
 		<div class="row">
 			<div class="col-sm-6">
-				<h3>Which code editor would you recommend?</h3>
-				<p>I'd highly recommend you <a href="http://www.sublimetext.com/">Sublime Text</a> - a free to try text editor which I'm using daily. Awesome tool!</p>
+				<h3>¿Qué editor de código recomendarías?</h3>
+				<p>Recomendaría encarecidamente <a href="http://www.sublimetext.com/">Sublime Text</a> - un editor de texto de prueba gratuita que uso a diario. ¡Una herramienta impresionante!</p>
 			</div>
 			<div class="col-sm-6">
-				<h3>Nice header. Where do I find more images like that one?</h3>
+				<h3>¿Dónde puedo encontrar más imágenes como esa?</h3>
 				<p>
-					Well, there are thousands of stock art galleries, but personally, 
-					I prefer to use photos from these sites: <a href="http://unsplash.com">Unsplash.com</a> 
-					and <a href="http://www.flickr.com/creativecommons/by-2.0/tags/">Flickr - Creative Commons</a></p>
+					Bueno, hay miles de galerías de arte de stock, pero personalmente, 
+					prefiero usar fotos de estos sitios: <a href="http://unsplash.com">Unsplash.com</a> 
+					y <a href="http://www.flickr.com/creativecommons/by-2.0/tags/">Flickr - Creative Commons</a></p>
 			</div>
 		</div> <!-- /row -->
 
 		<div class="row">
 			<div class="col-sm-6">
-				<h3>Can I use it to build a site for my client?</h3>
+				<h3>¿Puedo usarlo para construir un sitio para mi cliente?</h3>
 				<p>
-					Yes, you can. You may use this template for any purpose, just don't forget about the <a href="http://creativecommons.org/licenses/by/3.0/">license</a>, 
-					which says: "You must give appropriate credit", i.e. you must provide the name of the creator and a link to the original template in your work. 
+					Sí, puedes. Puedes usar esta plantilla para cualquier propósito, solo no olvides la <a href="http://creativecommons.org/licenses/by/3.0/">licencia</a>, 
+					que dice: "Debes dar crédito apropiado", es decir, debes proporcionar el nombre del creador y un enlace a la plantilla original en tu trabajo.
 				</p>
 			</div>
 			<div class="col-sm-6">
-				<h3>Can you customize this template for me?</h3>
-				<p>Yes, I can. Please drop me a line to sergey-at-pozhilov.com and describe your needs in details. Please note, my services are not cheap.</p>
+				<h3>¿Puedes personalizar esta plantilla para mí?</h3>
+				<p>Sí, puedo. Por favor, escríbeme a sergey-at-pozhilov.com y describe tus necesidades en detalle. Ten en cuenta que mis servicios no son baratos.</p>
 			</div>
 		</div> <!-- /row -->
 
 		<div class="jumbotron top-space">
 			<h4>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere magni ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia perspiciatis at voluptatibus. Libero, aliquid illum possimus numquam fuga.</h4>
-     		<p class="text-right"><a class="btn btn-primary btn-large">Learn more »</a></p>
+     		<p class="text-right"><a class="btn btn-primary btn-large">Aprender más »</a></p>
   		</div>
-
-</div>	<!-- /container -->
+	</div>	<!-- /Contenedor -->
 	
-	<!-- Social links. @TODO: replace by link/instructions in template -->
+	<!-- Enlaces sociales -->
 	<section id="social">
 		<div class="container">
 			<div class="wrapper clearfix">
-				<!-- AddThis Button BEGIN -->
+				<!-- Botón de AddThis -->
 				<div class="addthis_toolbox addthis_default_style">
 				<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
 				<a class="addthis_button_tweet"></a>
 				<a class="addthis_button_linkedin_counter"></a>
 				<a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
 				</div>
-				<!-- AddThis Button END -->
+				<!-- Fin del botón de AddThis -->
 			</div>
 		</div>
 	</section>
-	<!-- /social links -->
-
+	<!-- /Enlaces sociales -->
 
 	<footer id="footer" class="top-space">
-
 		<div class="footer1">
 			<div class="container">
 				<div class="row">
 					
 					<div class="col-md-3 widget">
-						<h3 class="widget-title">Contact</h3>
+						<h3 class="widget-title">Contacto</h3>
 						<div class="widget-body">
 							<p>+234 23 9873237<br>
 								<a href="mailto:#">some.email@somewhere.com</a><br>
@@ -190,7 +188,7 @@
 					</div>
 
 					<div class="col-md-3 widget">
-						<h3 class="widget-title">Follow me</h3>
+						<h3 class="widget-title">Sígueme</h3>
 						<div class="widget-body">
 							<p class="follow-me-icons">
 								<a href=""><i class="fa fa-twitter fa-2"></i></a>
@@ -202,14 +200,14 @@
 					</div>
 
 					<div class="col-md-6 widget">
-						<h3 class="widget-title">Text widget</h3>
+						<h3 class="widget-title">Widget de texto</h3>
 						<div class="widget-body">
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad id expedita cupiditate repellendus possimus unde?</p>
 							<p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat provident assumenda labore soluta minima alias temporibus facere distinctio quas adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate reprehenderit architecto sint libero illo et hic.</p>
 						</div>
 					</div>
 
-				</div> <!-- /row of widgets -->
+				</div> <!-- /Fila de widgets -->
 			</div>
 		</div>
 
@@ -220,11 +218,11 @@
 					<div class="col-md-6 widget">
 						<div class="widget-body">
 							<p class="simplenav">
-								<a href="#">Home</a> | 
-								<a href="about.html">About</a> |
-								<a href="sidebar-right.html">Sidebar</a> |
-								<a href="contact.html">Contact</a> |
-								<b><a href="signup.html">Sign up</a></b>
+								<a href="#">Inicio</a> | 
+								<a href="about.html">Acerca de</a> |
+								<a href="sidebar-right.html">Barra lateral</a> |
+								<a href="contact.html">Contacto</a> |
+								<b><a href="signup.html">Registrarse</a></b>
 							</p>
 						</div>
 					</div>
@@ -232,22 +230,17 @@
 					<div class="col-md-6 widget">
 						<div class="widget-body">
 							<p class="text-right">
-								Copyright &copy; 2014, Your name. Designed by <a href="http://gettemplate.com/" rel="designer">gettemplate</a> 
+								Derechos de autor &copy; 2014, Tu nombre. Diseñado por <a href="http://gettemplate.com/" rel="designer">gettemplate</a> 
 							</p>
 						</div>
 					</div>
 
-				</div> <!-- /row of widgets -->
+				</div> <!-- /Fila de widgets -->
 			</div>
 		</div>
-
-	</footer>	
-		
-
-
-
-
-	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	</footer>
+	
+	<!-- Los scripts de JavaScript se colocan al final del documento para que las páginas se carguen más rápido -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 	<script src="assets/js/headroom.min.js"></script>
